de86156313c74c87a7aa8032ac6ddf832119884e
Berel Levy <berellevy@gmail.com>
2020-05-20 16:55:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,6 +6,7 @@ def join_nested_strings(src)
     while element_index < src[row_index].length do
       if src[row_index][element_index].class == "text"
         string_array << src[row_index][element_index]
+      end
       element_index += 1
     end
     row_index += 1
